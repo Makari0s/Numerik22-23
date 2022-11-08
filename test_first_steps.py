3bc79553782d6082ce28dacdab1bf5e05a48d23c
Tommy <56879719+Makari0s@users.noreply.github.com>
2022-11-08 07:05:23 +0100

    Update test_first_steps.py

--- test_first_steps.py
+++ test_first_steps.py
@@ -11,7 +11,7 @@ class TestFirstSteps(TestCase):
         self.assertEqual(add(5, 3), 8)
 
     def test_subtract(self):
-        self.assertEqual(subtract(3, 5), 2)
+        self.assertEqual(subtract(5, 3), 2)
 
 
 if __name__ == "__main__":
